8ea149558ba1a002b6ffaf97848647d2d83747ad
Fabian <107806353+fabianislearning@users.noreply.github.com>
2023-12-03 22:50:48 +0800

    WS3 - Implementation 1 Completed

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
@@ -1,21 +1,24 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-// public class FirstLaw : MonoBehaviour
-// {
-//     public Vector3 force;
-//     Rigidbody rb;
+public class FirstLaw : MonoBehaviour
+{
+    public Vector3 force;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        //Get the RigidBody component which is attached to the GameObject.
+        rb = GetComponent<Rigidbody>();
+        //The force variable will affect where the ball will be located at via the transform (position) property
+        rb.transform.position = force;
+    }
 
-//     void Start()
-//     {
-//         rb = // your code here;
-//         rb.// your code here;
-//     }
-
-//     void FixedUpdate()
-//     {
-//         Debug.Log(transform.position);
-//     }
-// }
+    void FixedUpdate()
+    {
+        Debug.Log(transform.position);
+    }
+}
 
