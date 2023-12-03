a887af8cbc692152ab4842d2a4832e34e8452b2b
Fabian <107806353+fabianislearning@users.noreply.github.com>
2023-12-04 02:51:13 +0800

    WS3 - Implementation 3 Completed

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
@@ -1,37 +1,40 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class JumpToHeight : MonoBehaviour
-// {
-//     public float Height = 1f;
-//     Rigidbody rb;
+public class JumpToHeight : MonoBehaviour
+{
+    public float Height = 1f;
+    Rigidbody rb;
 
-//     private void Start()
-//     {
-//         rb = /*your code here*/;
-//     }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-//     void Jump()
-//     {
-//         // v*v = u*u + 2as
-//         // u*u = v*v - 2as
-//         // u = sqrt(v*v - 2as)
-//         // v = 0, u = ?, a = Physics.gravity, s = Height
+    void Jump()
+    {
+        // v*v = u*u + 2as
+        // u*u = v*v - 2as
+        // u = sqrt(v*v - 2as)
+        // v = 0, u = ?, a = Physics.gravity, s = Height
 
-//         float u = Mathf.Sqrt(/*your code here*/);
-//         rb.velocity = new Vector3(/*your code here*/);
+        /* Physics.gravity.y is a Vector that defines the default strength and direction for gravity.
+        We have a .y at the end because gravity acts in the vertical direction.*/
+        float u = Mathf.Sqrt(0 - 2 * Physics.gravity.y * Height);
+        rb.velocity = new Vector3(0, u, 0);
+        Debug.Log(rb.name + ": " + u);
 
-//         //float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
-//         //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-//     }
+        //float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
+        //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+    }
 
-//     private void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.Space))
-//         {
-//             /*your code here*/();
-//         }
-//     }
-// }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+}
 
