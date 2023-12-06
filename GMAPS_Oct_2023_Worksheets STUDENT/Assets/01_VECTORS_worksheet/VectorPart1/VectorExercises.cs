dbb4e447221f06d53ea9718924b4d6a529a68dcb
Fabian <107806353+fabianislearning@users.noreply.github.com>
2023-12-07 05:33:39 +0800

    WS1 - Q3(d) Completed

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/VectorPart1/VectorExercises.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/VectorPart1/VectorExercises.cs
@@ -141,12 +141,11 @@ public class VectorExercises : MonoBehaviour
         HVector2D c = new HVector2D(2, 2);
 
         HVector2D v1 = b - a;
-        // Your code here
 
-        //HVector2D proj = // Your code here
+        HVector2D proj = b * (c.DotProduct(v1) / b.DotProduct(b));
 
         DebugExtension.DebugArrow(a.ToUnityVector3(), b.ToUnityVector3(), Color.red, 60f);
         DebugExtension.DebugArrow(a.ToUnityVector3(), c.ToUnityVector3(), Color.yellow, 60f);
-        //DebugExtension.DebugArrow(a.ToUnityVector3(), proj.ToUnityVector3(), Color.white, 60f);
+        DebugExtension.DebugArrow(a.ToUnityVector3(), proj.ToUnityVector3(), Color.white, 60f);
     }
 }
