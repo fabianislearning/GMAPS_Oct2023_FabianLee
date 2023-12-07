ed1042a3c6566fef074d7285aeb06aa1108f66f1
Fabian <107806353+fabianislearning@users.noreply.github.com>
2023-12-07 15:57:46 +0800

    WS1 - Q6 WIP
    To go home and finish up.

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
@@ -5,52 +5,92 @@ using System.Linq;
 
 public class SoccerPlayer : MonoBehaviour
 {
-    //public bool IsCaptain = false;
-    //public SoccerPlayer[] OtherPlayers;
-    //public float rotationSpeed = 1f;
+    public bool IsCaptain = false;
+    public SoccerPlayer[] OtherPlayers;
+    public float rotationSpeed = 1f;
 
-    //float angle = 0f;
+    float angle = 0f;
 
-    //private void Start()
-    //{
-       
-    //}
+    private void Start()
+    {
+        OtherPlayers = FindObjectsOfType<SoccerPlayer>().Where(t => t != this).ToArray();
+
+        /* OtherPlayers = FindObjectsOfType<SoccerPlayer>();
+        SoccerPlayer[] temp = new SoccerPlayer[OtherPlayers.Length - 1];
+        int i = 0;
+        foreach (SoccerPlayer p in OtherPlayers)
+        {
+            if (p != this)
+            {
+                temp[i] = p;
+                i++;
+            }
+        }
+        OtherPlayers = temp;
+        Debug.Log(OtherPlayers.Length);*/
+    }
 
     //float Magnitude(Vector3 vector)
     //{
-        
+
     //}
 
     //Vector3 Normalise(Vector3 vector)
     //{
-        
+
     //}
 
     //float Dot(Vector3 vectorA, Vector3 vectorB)
     //{
-        
-    //}
-
-    //SoccerPlayer FindClosestPlayerDot()
-    //{
-    //    SoccerPlayer closest = null;
-
-    //    return closest;
-    //}
-
-    //void DrawVectors()
-    //{
-    //    foreach (SoccerPlayer other in OtherPlayers)
-    //    {
-    //        // Your code here
-    //        // ...
-    //    }
-    //}
-
-    //void Update()
-    //{
 
     //}
+
+    SoccerPlayer FindClosestPlayerDot()
+    {
+        SoccerPlayer closest = null;
+        float minAngle = 180f;
+
+        for (int i = 0; i < OtherPlayers.Length; i++)
+        {
+            Vector3 toPlayer = OtherPlayers[i].transform.position;
+            closest = Normalise(toPlayer);
+
+            float dot = Dot(toPlayer);
+            float angle = Mathf.Acos(dot);
+            angle = angle * Mathf.Rad2Deg;
+
+            if (angle < minAngle)
+            {
+                minAngle = angle;
+                closest = transform.position;
+            }
+        }
+
+        return closest;
+    }
+
+    void DrawVectors()
+    {
+        foreach (SoccerPlayer other in OtherPlayers)
+        {
+            // Your code here
+            // ...
+        }
+    }
+
+    void Update()
+    {
+        DebugExtension.DebugArrow(transform.position, transform.forward, Color.red);
+
+        if (IsCaptain)
+        {
+            angle += Input.GetAxis("Horizontal") * rotationSpeed;
+            transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up);
+            Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
+        }
+
+        DrawVectors();
+    }
 }
 
 
